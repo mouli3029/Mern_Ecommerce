2f54031e655ffe2478676861ebfcd36379a26c6c
Mouli <venkatmouli823@gmail.com>
2020-12-18 20:16:23 +0530

    Validation Part 2

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -7,7 +7,7 @@ const {signout ,signup} = require('../controllers/auth')
 router.post('/signup', [
     check('name',"Name should be atleast 3 characters").isLength({min : 3}),
     check('email',"Email is required").isEmail(),
-    check('password',"Password should be atleast 3 characters").length({min : 3})
+    check('password',"Password should be atleast 3 characters").isLength({min : 3})
 ], signup);
 router.get('/signout',signout);
 
