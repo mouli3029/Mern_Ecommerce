f4f69610e053100fc2c508ea1a650f122300f37d
Mouli <venkatmouli823@gmail.com>
2020-12-18 20:06:16 +0530

    Validation -- express-validator

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 const {signout ,signup} = require('../controllers/auth')
 
 
-router.post('/signup', signup);
+router.post('/signup', [
+    check('name',"Name should be atleast 3 characters").isLength({min : 3}),
+    check('email',"Email is required").isEmail(),
+    check('password',"Password should be atleast 3 characters").length({min : 3})
+], signup);
 router.get('/signout',signout);
 
 module.exports = router;
