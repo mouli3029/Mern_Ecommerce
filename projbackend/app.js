fe44e4c21e21740aa55fa0b5df15daa4111e9300
Mouli <venkatmouli823@gmail.com>
2020-12-18 16:55:08 +0530

    Common Middlewares

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
+const express      = require('express');
+const mongoose     = require('mongoose');
+const bodyParser   = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors         = require('cors');
+
 
 const app = express();
 
@@ -16,6 +20,10 @@ mongoose.connect(process.env.DATABASE, {
 .catch((err)=>{
     console.log(err);
 })
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 //run from env or run at 8000 port
 const port = process.env.PORT || 8000;
