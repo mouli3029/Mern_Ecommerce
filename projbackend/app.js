fee8fa5a5bcebe6635cf092353e7203a67f0baec
Mouli <venkatmouli823@gmail.com>
2020-12-18 16:29:57 +0530

    Database Connectivity

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,1 +1,27 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex : true
+})
+.then((db)=>{
+    console.log('DB CONNECTED!!');
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+//run from env or run at 8000 port
+const port = process.env.PORT || 8000;
+
+app.listen(port,()=>{
+    console.log(`App is running at ${port}`);
+})
+
+//We can also use mysql (node module mysql).
