e53b9298b2adc9cfa92cdfaf8010021e3af23d92
Mouli <venkatmouli823@gmail.com>
2020-12-24 22:33:00 +0530

    Connecting Backend with Frontend

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
+import '../styles.css';
+import {API} from '../backend';
+
 
 function Home() {
+    console.log("API is",API);
     return (
         <div>
-            <h1>Hello front end</h1>
+            <h1 className="text-white">Hello front end</h1>
         </div>
     )
 }
