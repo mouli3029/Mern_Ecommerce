ee98c5898062358e07f7afec3a7a0f991ff7390d
Mouli <venkatmouli823@gmail.com>
2020-12-25 18:07:37 +0530

    SignUp

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,8 +1,49 @@
 import React,{useState} from 'react';
 import Base from '../core/Base';
 import {Link} from 'react-router-dom'
+import { signup } from '../auth/helper';
 
 const Signup=()=> {
+
+    const [values,setValues] = useState({
+        name : "",
+        email:"",
+        password : "",
+        error : "",
+        success : false
+    })
+
+    const {name,email,password,success,error} = values;
+
+    const handleChange = name => event =>{
+/*         [name] changes according to the value we passs through the handleChange
+ */        setValues({...values,error:false, [name]:event.target.value});
+           
+    } 
+
+    const onSubmit = event => {
+        //prevents redirecting when we click
+        event.preventDefault();
+
+        setValues({...values,error:false})
+        signup({name,email,password})
+        .then(data =>{
+            if(data.error){
+                setValues({...values,error: data.error , success : false})
+            }
+            else{
+                setValues({
+                    ...values,
+                    name : "",
+                    email : '',
+                    password : "",
+                    error : "",
+                    success : true
+                })
+            }
+        })
+        .catch(console.log("Error in Signup"))
+    }
     const signUpForm = () =>{
         return (
             <div className="row">
@@ -10,27 +51,54 @@ const Signup=()=> {
                     <form>
                         <div className="form-group">
                             <label className="text-light">Name</label>
-                            <input className="form-control" type ="text"/>
+                            <input className="form-control" onChange={handleChange("name")} type ="text" value={name}/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" type ="email"/>
+                            <input className="form-control" onChange={handleChange("email")} type ="email" value={email}/>
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" type ="password"/>
+                            <input className="form-control" type ="password" onChange={handleChange("password")} value={password}/>
                         </div>
-                        <button className="btn btn-success btn-block">Submit</button>
+                        <button onClick = {onSubmit} className="btn btn-success btn-block">Submit</button>
                     </form>
                 </div>
             </div>
         )
     }
 
+    const successMessage = ()=>{
+        return(
+        <div className="row">
+        <div className="col-md-6 offset-md-3 text-left">
+        <div className="alert alert-success" style={{display : success ? "" :"none"}}>
+            New account was created Successfully. Please <Link to ="/signin">Login here</Link>
+        </div>
+        </div>
+        </div>
+        )}
+    const errorMessage = ()=>{
+        return (
+     <div className="row">
+    <div className="col-md-6 offset-md-3 text-left">
+        <div className="alert alert-danger" style={{display : error ? "" :"none"}}>
+           {error}
+        </div>
+        </div> </div>
+        
+        
+        )}
+
     return (
         <div>
             <Base title='Sign up Page' description="A page for user to Signup">
+                {successMessage()}
+                {errorMessage()}
               {signUpForm()}
+              <p className='text-white text-center'>
+                  {JSON.stringify(values)}
+              </p>
             </Base>
         </div>
     )
