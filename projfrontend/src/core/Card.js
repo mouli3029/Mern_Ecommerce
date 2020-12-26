ef54fab22659b3046e98fa6fa64866f5f15203d5
Mouli <venkatmouli823@gmail.com>
2020-12-26 11:02:00 +0530

    Home page

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,38 +1,62 @@
 import React from 'react'
 import ImageHelper from './helper/ImageHelper';
 const Card = ({
-    product
+    product,
+    AddtoCart = true,
+    removeFromCart = false,
 }) => {
-        return (
-          <div className="card text-white bg-dark border border-info ">
-            <div className="card-header lead">A photo from pexels</div>
-            <div className="card-body">
-              <ImageHelper product={product} />
-              <p className="lead bg-success font-weight-normal text-wrap">
-                this photo looks great
-              </p>
-              <p className="btn btn-success rounded  btn-sm px-4">$ 5</p>
-              <div className="row">
-                <div className="col-12">
-                  <button
-                    onClick={() => {}}
-                    className="btn btn-block btn-outline-success mt-2 mb-2"
-                  >
-                    Add to Cart
-                  </button>
-                </div>
-                <div className="col-12">
-                  <button
+
+    const cardTitle = product ? product.name :"Photo from pexels";
+    const cardDescription = product ? product.description :"Default description";
+    const cardPrice = product ? product.price :"DEFAULT";
+
+
+
+    const showAddToCart = () => {
+        return(
+            AddtoCart && (
+                <button
+                onClick={() => {}}
+                className="btn btn-block btn-outline-success mt-2 mb-2"
+              >
+                Add to Cart
+              </button>
+            )
+        )
+
+    }
+    const showRemoveFromCart = removeFromCart => {
+        return(
+            removeFromCart && (
+                <button
                     onClick={() => {}}
                     className="btn btn-block btn-outline-danger mt-2 mb-2"
                   >
                     Remove from cart
                   </button>
+            )
+        )
+        
+    }
+        return (
+          <div className="card text-white bg-dark border border-info ">
+            <div className="card-header lead">{cardTitle}</div>
+            <div className="card-body">
+              <ImageHelper product={product} />
+              <p className="lead bg-success font-weight-normal text-wrap">
+                {cardDescription}
+              </p>
+              <p className="btn btn-success rounded  btn-sm px-4">{cardPrice}</p>
+              <div className="row">
+                <div className="col-12">
+                 {showAddToCart(AddtoCart)}
+                </div>
+                <div className="col-12">
+                  {showRemoveFromCart(removeFromCart)}
                 </div>
               </div>
             </div>
           </div>
         );
 };
-
 export default Card
--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,26 +1,44 @@
-import React from 'react';
+import React,{useState,useEffect}from 'react';
 import '../styles.css';
 import {API} from '../backend';
 import Base from './Base';
 import Card from './Card';
+import { getProducts } from './helper/coreapicalls';
 
 
 
 
 function Home() {
-    console.log("API is",API);
+    const [products, setProducts] = useState([]);
+    const [error,setError] =  useState(false);
+
+    const loadAllproducts = () => {
+        getProducts().then(data => {
+            if(data.error){
+                setError(data.error);
+            }
+            else{
+                setProducts(data);
+            }
+        })
+    }
+    useEffect(() => {
+        loadAllproducts()
+    }, [])
     return (
         <Base title="Home Page" description="Welcome to Tshirt Store">
            <div className="row">
-               <div className="col-4 text-center">
-                  <Card/>
-               </div>
-               <div className="col-4">
-                   <button className='btn-success'>Test</button>
-               </div>
-               <div className="col-4">
-                   <button className='btn-success'>Test</button>
-               </div>
+              <h1 className='text-white'>All of Tshirts</h1>
+              <div className="row">
+                  {products.map((product,index)=>{
+                      return(
+                          <div key ={index} className="col-4 mb-4">
+                              <Card product={product}/>
+
+                        </div>
+                      )
+                  })}
+              </div>
            </div>
         </Base>
           
